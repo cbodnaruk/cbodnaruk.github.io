d729044b15ab81a4e9e5238d437c6236c7349b05
cbodnaruk <91246921+cbodnaruk@users.noreply.github.com>
2025-08-15 12:41:53 +1000

    Accessibility changes and mobile view fixes

--- burgermenu.js
+++ burgermenu.js
@@ -6,6 +6,17 @@ function initBurgerMenu() {
 
     menuButton.addEventListener('click', () => {
         menu.classList.toggle('open');
+        if (menu.classList.contains('open')) document.body.addEventListener('click',closeBurger)
     });
 
+}
+
+function closeBurger(e){
+    const menuButton = document.querySelector('#menu-button');
+    const menu = document.querySelector('.header-buttons');
+    if (e.target != menu && e.target != menuButton){
+        menu.classList.remove('open')
+        document.body.removeEventListener('click',closeBurger)
+
+    }
 }
